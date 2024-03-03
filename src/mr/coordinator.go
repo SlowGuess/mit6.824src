c83ff42895c4d1bd74565ad139d2c0345d8dd5ca
SlowGuess <245900981@qq.com>
2024-03-03 18:26:52 +0800

    第一次实验_1

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,150 @@
 package mr
 
-import "log"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"sync"
+	"sync/atomic"
+	"time"
+)
 import "net"
 import "os"
 import "net/rpc"
 import "net/http"
 
-
 type Coordinator struct {
 	// Your definitions here.
+	Files         []string
+	ReduceNumber  int
+	MapNumber     int
+	MapJobList    []*Job
+	ReduceJobList []*Job
+	CurrentStates int32 // 所处的阶段枚举， 1-map阶段、2-reduce阶段、3-都完成[程序可退出]、4-需等待
+	JobListMutex  sync.Mutex
+}
 
+type Job struct {
+	//static information
+	FileName     string //任务文件名
+	ListIndex    int    //在任务列表中的下标
+	ReduceID     int    // reduce 任务号  从0-(N-1) 和上面一个属性重复了
+	ReduceNumber int    //Reduce数量个数
+	JobType      int    //任务类型 map or reduce
+
+	//dynamic information
+	JobFinished    bool  //是否正确完成
+	StartTime      int64 //任务的分配时间[初始为0]，如果下次检查是否可分配时超过2s，就当失败处理，重新分配
+	FirstStartTime int64 // 最终完成的时间
+	FetchCount     int   // 任务分配次数，用于统计信息
+	// 规定： map产生的中间文件的名字格式是  输入文件名_分区号    reduce 产生的最终文件是 mr-out-reduce任务号
+}
+
+const (
+	// InReduce 当前系统所处的mr阶段
+	InReduce    = 1
+	InMap       = 2
+	AllFinished = 3
+	// JobTimeoutSecond 任务的超时时间，超过 xx 当做客户端crash了，重新分配任务。 如果设置的太小，则 map count 检测程序不通过。
+	JobTimeoutSecond = 5
+	// MapJob 任务类型
+	MapJob    = 1
+	ReduceJob = 2
+	// BackgroundInterval 后台线程的运行间隔
+	BackgroundInterval = 500 * time.Millisecond
+	// MasterLogPrefix 日志前缀
+	MasterLogPrefix = "master_log: "
+	WorkerLogPrefix = "worker_log: "
+)
+
+// 序列化一个结构体对象, 打印日志时候用
+func toJsonString(inter interface{}) string {
+	bytes, _ := json.Marshal(inter)
+	return string(bytes)
+}
+
+type JobFetchReq struct {
+	// 不需要有东西。如果服务端需要记录客户端信息，可以传入客户端id等信息，微服务mesh层应该做的事情。
+}
+
+type JobFetchResp struct {
+	NeedWait bool // 是否需要等待下次轮询任务， 因为服务端可能已经分发完map任务，但Map阶段还没结束[map任务正在被执行]
+	Job           // 继承写法,相当于把Job里面的所有属性写到这里
+}
+
+// JobDoneReq 任务完成提交的请求体
+type JobDoneReq struct {
+	Job // 继承写法,相当于把Job结构体里面的所有属性写到这里
+}
+
+// JobDoneResp 任务完成提交的返回体
+type JobDoneResp struct {
+	// 不需要有额外信息
 }
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
-func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
+func (m *Coordinator) JobFetch(req *JobFetchReq, resp *JobFetchResp) error {
+	m.JobListMutex.Lock()
+	defer m.JobListMutex.Unlock()         //上锁
+	CurrentTime := time.Now().UnixMilli() //获取当前时间
+	//分配任务
+	JobList := m.MapJobList
+	switch m.CurrentStates {
+	case InReduce:
+		JobList = m.ReduceJobList
+	case InMap:
+		JobList = m.MapJobList
+	case AllFinished:
+		return nil
+	}
+	for _, job := range JobList {
+		//任务没完成，且是第一次运行或之前超时
+		if !job.JobFinished && (job.StartTime == 0 || (CurrentTime-job.StartTime)/1000 > int64(JobTimeoutSecond)) {
+			job.FetchCount++
+			job.StartTime = CurrentTime
+			//记录第一次运行的时间
+			if job.FirstStartTime == 0 {
+				job.FirstStartTime = CurrentTime
+			}
+			// 赋值给resp，即分发给请求源:客户端
+			fmt.Printf(logTime()+MasterLogPrefix+"—————————————— 分发出任务:%v \n", job.ListIndex)
+			resp.Job = *job
+			return nil
+		}
+	}
+	// 只要不是所有任务都是已完成状态，就让work继续等，否则无法通过early_exit测试程序
+	if m.CurrentStates != AllFinished {
+		resp.NeedWait = true
+	}
 	return nil
 }
 
+// JobDone 客户端提交任务的处理函数
+func (m *Coordinator) JobDone(req *JobDoneReq, resp *JobDoneResp) error {
+	m.JobListMutex.Lock()
+	defer m.JobListMutex.Unlock()
+	finished := req.JobFinished
+	jobType := "Map任务"
+	if req.JobType == ReduceJob {
+		jobType = "Reduce任务"
+	}
+	fmt.Printf(logTime()+MasterLogPrefix+"—————————————— 完成%s任务耗时:%v 毫秒[从第一次被分发], %v毫秒[最近一次]，分配次数%v —————————————— \n",
+		jobType, time.Now().UnixMilli()-req.FirstStartTime, time.Now().UnixMilli()-req.StartTime, req.FetchCount)
+	switch m.CurrentStates {
+	// 将任务的状态改为是否已完成，后台定时线程会扫描此状态
+	case InMap:
+		m.MapJobList[req.ListIndex].JobFinished = finished
+	case InReduce:
+		m.ReduceJobList[req.ListIndex].JobFinished = finished
+	}
+	return nil
+}
 
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,30 +158,106 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
-
 	// Your code here.
-
-
+	for atomic.LoadInt32(&c.CurrentStates) != AllFinished {
+		time.Sleep(300 * time.Millisecond)
+	}
+	ret = true
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
-
 	// Your code here.
-
-
+	c.InitCoordinator(files, nReduce)
+	go c.Background()
 	c.server()
 	return &c
+}
+
+// Background 后台扫描，任务是否都完成，是否有卡死的任务，是否进入下一个阶段，每隔100毫秒扫描一次
+func (m *Coordinator) Background() {
+	// 保证原子性，使用 atomic.LoadInt 进行判断
+	for atomic.LoadInt32(&m.CurrentStates) != AllFinished {
+		// 循环遍历任务
+		m.JobListMutex.Lock()
+		isAllJobDone := true
+		leftCount := 0
+		switch m.CurrentStates {
+		case InMap:
+			for _, job := range m.MapJobList {
+				if !job.JobFinished {
+					isAllJobDone = false
+					leftCount++
+				}
+			}
+			fmt.Printf(logTime()+MasterLogPrefix+"—————————————— 还剩 %v 个 map 任务\n", leftCount)
+			// map 任务都做完了，流转到 reduce 状态, 而且要生成 reduce 任务
+			if isAllJobDone {
+				leftCount = 0
+				atomic.StoreInt32(&m.CurrentStates, InReduce)
+				m.generateReduceMap()
+				fmt.Printf(MasterLogPrefix+"background: CurrentStates change from %v to %v\n", InMap, InReduce)
+			}
+		case InReduce:
+			for _, job := range m.ReduceJobList {
+				if !job.JobFinished {
+					isAllJobDone = false
+					leftCount++
+				}
+			}
+			fmt.Printf(logTime()+MasterLogPrefix+"—————————————— 还剩 %v 个 reduce 任务\n", leftCount)
+			// reduce 任务都做完了，流转到 结束 状态
+			if isAllJobDone {
+				atomic.StoreInt32(&m.CurrentStates, AllFinished)
+				fmt.Printf(MasterLogPrefix+"background: CurrentStates change from %v to %v\n", InReduce, AllFinished)
+			}
+		}
+		m.JobListMutex.Unlock()
+		time.Sleep(5 * BackgroundInterval)
+	}
+}
+
+// 生成reduce任务，调用前需要持有锁
+func (m *Coordinator) generateReduceMap() {
+	reduceJobList := make([]*Job, 0, m.ReduceNumber)
+	for i := 0; i < m.ReduceNumber; i++ {
+		reduceJobList = append(reduceJobList, &Job{
+			ListIndex:    i,
+			ReduceID:     i,
+			ReduceNumber: m.ReduceNumber,
+			JobType:      ReduceJob,
+		})
+	}
+	m.ReduceJobList = reduceJobList
+	fmt.Printf(MasterLogPrefix+" generateReduceMap finished,m.ReduceJobList: %+v\n", toJsonString(reduceJobList))
+}
+
+// InitCoordinator 初始化函数
+func (m *Coordinator) InitCoordinator(files []string, nReduce int) {
+	m.Files = files
+	m.ReduceNumber = nReduce
+	m.CurrentStates = InMap
+	m.initMapJob()
+}
+
+// 初始化 Map 任务
+func (m *Coordinator) initMapJob() {
+	m.MapJobList = make([]*Job, 0, len(m.Files))
+	for i, file := range m.Files {
+		m.MapJobList = append(m.MapJobList, &Job{
+			JobType:      MapJob,
+			FileName:     file,
+			ListIndex:    i,
+			ReduceNumber: m.ReduceNumber,
+		})
+	}
+	fmt.Printf(MasterLogPrefix+"master init finished %+v\n", toJsonString(m))
 }
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,77 +1,170 @@
 package mr
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+	"strings"
+	"time"
+)
 import "log"
 import "net/rpc"
 import "hash/fnv"
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
+func (job *Job) DoMapJob(mapf func(string, string) []KeyValue) error {
+	// 读取文件内容
+	content, err := job.ReadFile(job.FileName)
+	if err != nil {
+		return err
+	}
+	// 调用mapf函数，将文件内容转换为键值对
+	kva := mapf(job.FileName, content)
 
-//
+	// 根据键的哈希值确定对应的分区号
+	partition := make([][]KeyValue, job.ReduceNumber)
+	for _, kv := range kva {
+		partition_index := ihash(kv.Key) % job.ReduceNumber
+		// 将生成的键值对按照 Reduce 任务数量分组
+		// 将 kv 存放到对应的分组中
+		partition[partition_index] = append(partition[partition_index], kv)
+	}
+
+	for i := 0; i < job.ReduceNumber; i++ {
+		//使分区内部有序
+		sort.Sort(ByKey(partition[i]))
+
+		//将map工作完成的内容放在本地，供reduce工作使用
+		map_filename := fmt.Sprintf("worker-file%d-map-%03d-out.txt", job.ListIndex, i)
+		file, err := os.OpenFile(map_filename, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, os.ModePerm)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		// 写入文件
+		for _, kv := range partition[i] {
+			fmt.Fprintf(file, "%s %s\n", kv.Key, kv.Value)
+		}
+	}
+	return nil
+}
+
+func (job *Job) DoReduceJob(reducef func(string, []string) string) error {
+
+}
+
+func (job *Job) ReadFile(filename string) (string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return "", fmt.Errorf("cannot open file: %v", err)
+	}
+	defer file.Close()
+
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		return "", fmt.Errorf("cannot read file: %v", err)
+	}
+	return string(content), nil
+}
+
+// 多路归并，找最小值
+func findMinIndex(keyValueList2 [][]*KeyValue, indexList []int) int {
+
+}
+
+// 返回当前时间戳， 打印日志时候用
+func logTime() string {
+	return fmt.Sprint((time.Now().UnixNano()/1e6)%10000, " ")
+}
+
+func CallFetchJob() JobFetchResp {
+	req := JobFetchReq{}
+	resp := JobFetchResp{}
+	call("Coordinator.JobFetch", &req, &resp)
+	//fmt.Printf(WorkerLogPrefix+"CallFetchJob job resp %+v\n", resp)
+	return resp
+}
+
+func CallCommitJob(job *JobDoneReq) {
+	//fmt.Printf(WorkerLogPrefix+"CallCommitJob job req %+v\n", *job)
+	resp := JobDoneResp{}
+	call("Coordinator.JobDone", job, &resp)
+}
+
+// DoJob 开始工作
+func (job *Job) DoJob(mapf func(string, string) []KeyValue, reducef func(string, []string) string) {
+	var err error
+	switch job.JobType {
+	case MapJob:
+		if err = job.DoMapJob(mapf); err != nil {
+			fmt.Println("DoMapJob_error ", err)
+		}
+	case ReduceJob:
+		if err = job.DoReduceJob(reducef); err != nil {
+			fmt.Println("DoReduceJob_error ", err)
+		}
+	}
+	// 成功做完修改状态
+	if err == nil {
+		job.JobFinished = true
+	}
+	fmt.Printf(WorkerLogPrefix+"DoMapJob_finished %v\n ", toJsonString(job))
+}
+
 // main/mrworker.go calls this function.
-//
-func Worker(mapf func(string, string) []KeyValue,
-	reducef func(string, []string) string) {
-
+func Worker(mapf func(string, string) []KeyValue, reducef func(string, []string) string) {
 	// Your worker implementation here.
-
+	startTime := int64(0)
+	for true {
+		startTime = time.Now().UnixNano()
+		// 索要任务，得到的可能是 Map 或者 Reduce 的任务
+		job := CallFetchJob()
+		// 需要等待流转到 reduce
+		if job.NeedWait {
+			time.Sleep(5 * BackgroundInterval)
+			continue
+		}
+		// 任务都做完了，停止循环
+		if job.FetchCount == 0 {
+			fmt.Println(logTime() + WorkerLogPrefix + "任务都做完了，worker退出")
+			break
+		}
+		// 做任务
+		job.DoJob(mapf, reducef)
+		// 做完了，提交
+		CallCommitJob(&JobDoneReq{job.Job})
+		//fmt.Println(WorkerLogPrefix+"一次worker循环耗时[毫秒]:", (time.Now().UnixNano()-startTime)/1e6)
+		startTime++
+	}
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
 
 }
 
-//
-// example function to show how to make an RPC call to the coordinator.
-//
-// the RPC argument and reply types are defined in rpc.go.
-//
-func CallExample() {
-
-	// declare an argument structure.
-	args := ExampleArgs{}
-
-	// fill in the argument(s).
-	args.X = 99
-
-	// declare a reply structure.
-	reply := ExampleReply{}
-
-	// send the RPC request, wait for the reply.
-	// the "Coordinator.Example" tells the
-	// receiving server that we'd like to call
-	// the Example() method of struct Coordinator.
-	ok := call("Coordinator.Example", &args, &reply)
-	if ok {
-		// reply.Y should be 100.
-		fmt.Printf("reply.Y %v\n", reply.Y)
-	} else {
-		fmt.Printf("call failed!\n")
-	}
-}
-
-//
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
